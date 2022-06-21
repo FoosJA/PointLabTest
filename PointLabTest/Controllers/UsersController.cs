dab0772528cef67ab5cb7fa7964f07be954a8d02
Foos <JuliaAlekseevna6797@gmail.com>
2022-06-21 22:05:40 +0400

    Настройка запросов POST

--- PointLabTest/Controllers/UsersController.cs
+++ PointLabTest/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using PointLabTest.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,25 +14,61 @@ namespace PointLabTest.Controllers
 	public class UsersController : ControllerBase
 	{
 		private AppDataBaseContext _ctx;
-		public void ValuesController(AppDataBaseContext ctx) => _ctx = ctx;
+		public UsersController(AppDataBaseContext ctx)
+		{ _ctx = ctx; }
 
-		[HttpGet]
+
+		[HttpGet("test")]
 		public IEnumerable<string> Get()
 		{
 			return new string[] { "val1", "val2" };
 		}
 
-		[HttpPost]
-		public string Post([FromBody] string value)
+		[HttpPost("register")]
+		public IActionResult Post(string username, string password, string email)
 		{
-			return value + "test";
-			/*User user = new User();
-			user.UserName = value;
-			user.Email = 20;
+			User user = new User() { UserName = username, Email = email, Password = password };
+			var validCtx = new ValidationContext(user);
+			var validRst = new List<ValidationResult>();
+			if (!Validator.TryValidateObject(user, validCtx, validRst, true))
+			{
+				string info = "";
+				foreach (var error in validRst)
+				{
+					info += error.ErrorMessage;
+				}
+				Response.StatusCode = 400;
+				return new JsonResult(new MyResponse() { Success = false, Message = info });
+			}
 
+			if (_ctx.Users.Any(x => x.Email == user.Email))
+			{
+				Response.StatusCode = 400;
+				return new JsonResult(new MyResponse() { Success = false, Message = "Пользователь с таким email уже существует" });
+			}
 
-			_ctx.Users.Add(user); //Insert
-			_ctx.SaveChanges();   //commint*/
+			_ctx.Users.Add(user);
+			_ctx.SaveChanges();
+			return new JsonResult(new MyResponse() { Success = true, Message = "Успешный успех" });
+		}
+		[HttpPost("login")]
+		public IActionResult Post(string username, string password)
+		{
+			if(username is null || password is null)
+			{
+				Response.StatusCode = 400;
+				return new JsonResult(new MyResponse() { Success = false, Message = "Некорректный запрос" });
+			}
+			try//TODO: Ответ должен содержать класс userdata с полями пользователя
+			{
+				User user = _ctx.Users.Single(x => x.UserName == username && x.Password == password);
+				return new JsonResult(new MyResponse() { Success = true, Message = "Успешный успех" });
+			}
+			catch
+			{
+				Response.StatusCode = 401;
+				return new JsonResult(new MyResponse() { Success = false, Message = "Пользователь не найден" });
+			}					
 		}
 	}
 }
--- PointLabTest/Models/User.cs
+++ PointLabTest/Models/User.cs
@@ -1,11 +1,22 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace PointLabTest.Models
 {
 	public class User
 	{
 		public int Id { get; set; }
-		public string UserName{ get; set; }
+
+		[Required]
+		[StringLength(50, MinimumLength = 1)]
+		public string UserName { get; set; }
+
+		[Required]
+		[StringLength(50, MinimumLength = 4)]
 		public string Password { get; set; }
-		public string Email{ get; set; }
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
 	}
 }
